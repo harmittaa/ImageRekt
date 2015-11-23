214e49113a3d10be9dc47b1dcea273dd10b24fb7
Matti <Matti>
2015-11-23 12:31:20 +0200

    query for user images working

--- src/java/imageRektDB/GenericResource.java
+++ src/java/imageRektDB/GenericResource.java
@@ -5,6 +5,8 @@
  */
 package imageRektDB;
 
+import static java.lang.System.out;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -29,6 +31,7 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("generic")
 public class GenericResource {
+
     EntityManagerFactory emf;
     EntityManager em;
     private int i = 0;
@@ -40,7 +43,9 @@ public class GenericResource {
     private int userPK = 0;
     private int userSearchTerm = 0;
     private List<String> imagePaths;
-
+    private List<Image> imageQuery;
+    private String imagePath = "";
+    private User queryUser;
 
     @Context
     private UriInfo context;
@@ -53,9 +58,9 @@ public class GenericResource {
 
     /**
      * Retrieves representation of an instance of imageRektDB.GenericResource
+     *
      * @return an instance of java.lang.String
      */
-    
     @GET
     @Path("unused")
     @Produces("application/xml")
@@ -63,33 +68,32 @@ public class GenericResource {
         //TODO return proper representation object
         throw new UnsupportedOperationException();
     }
-    
-   /* @POST
-    @Path("test")
-    @Consumes("multipart/form-data")
-    public String testMethod() {
-        emf = Persistence.createEntityManagerFactory("ImageRektPU");
-        em = emf.createEntityManager();
-        em.getTransaction().begin();
-        this.userPK = Integer.parseInt("15");
-        User newUser = new User(this.userPK);
-        newUser.setUname("unamefromtest");
-        newUser.setUpass("passfromtest");
-        newUser.setUemail("fromtestmethod");
-        em.persist(newUser);
-        em.getTransaction().commit();
-        return "filename";
-    }
-    */
-    
+
+    /* @POST
+     @Path("test")
+     @Consumes("multipart/form-data")
+     public String testMethod() {
+     emf = Persistence.createEntityManagerFactory("ImageRektPU");
+     em = emf.createEntityManager();
+     em.getTransaction().begin();
+     this.userPK = Integer.parseInt("15");
+     User newUser = new User(this.userPK);
+     newUser.setUname("unamefromtest");
+     newUser.setUpass("passfromtest");
+     newUser.setUemail("fromtestmethod");
+     em.persist(newUser);
+     em.getTransaction().commit();
+     return "filename";
+     }
+     */
     //for registering a new user from index.html
     @POST
     @Path("registerUser")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public String registerUser(@FormParam("username") String username,
-                                @FormParam("email") String email,
-                                @FormParam("password") String password,
-                                @FormParam("pk") String pk){
+            @FormParam("email") String email,
+            @FormParam("password") String password,
+            @FormParam("pk") String pk) {
         emf = Persistence.createEntityManagerFactory("ImageRektPU");
         em = emf.createEntityManager();
         em.getTransaction().begin();
@@ -100,7 +104,7 @@ public class GenericResource {
         newUser.setUemail(email);
         em.persist(newUser);
         em.getTransaction().commit();
-        
+
         // create query for finding the user just created
         for (User p : (List<User>) em.createQuery("SELECT c FROM User c WHERE c.uid LIKE :UID").setParameter("UID", this.userPK).getResultList()) {
             return "Found just created user from database " + p.getUname() + " with email " + p.getUemail();
@@ -110,9 +114,7 @@ public class GenericResource {
         //if something goes wrong
         return "Somethign went wrong! " + newUser.getUname() + " " + newUser.getUemail();
     }
-    
-    
-    //doesnt work, boo hoo
+
     @POST
     @Path("findUserImages")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
@@ -120,16 +122,36 @@ public class GenericResource {
         emf = Persistence.createEntityManagerFactory("ImageRektPU");
         em = emf.createEntityManager();
         em.getTransaction().begin();
+
         this.userSearchTerm = Integer.parseInt(uid);
-        for (Image i : (List<Image>) em.createQuery("SELECT c FROM Image WHERE c.uid LIKE :UID").setParameter("UID", this.userSearchTerm).getResultList()) {
-            this.imagePaths.add(i.getPath());
-            return i.getPath() + "<br>";
+        for (User p : (List<User>) em.createQuery("SELECT c FROM User c WHERE c.uid LIKE :UID").setParameter("UID", this.userSearchTerm).getResultList()) {
+            queryUser = p;
         }
+
+        // doesn work
+        /*        for (Image q : (List<Image>) em.createQuery("SELECT i FROM Image i WHERE i.uid LIKE :uid").setParameter("uid", queryUser).getResultList()) {
+        return "Found image desc " + q.getDescription() + " with path " + q.getPath();
+        }*/
+        imageQuery = em.createNamedQuery("Image.findAll").getResultList();
+        ArrayList<Image> userImages = new ArrayList();
+        for (Image i : imageQuery) {
+            if (i.getUid() == queryUser) {
+                userImages.add(i);
+            }
+        }
+
+        //works
+        /*  for (Image u : (List<Image>) em.createNamedQuery("Image.findAll").getResultList()) {
+         if (u.getUid() == queryUser) {
+         return "Found image http://192.168.56.1/test/" + u.getPath() + " <br>";
+         }
+         } */
         em.getTransaction().commit();
         emf.close();
-        return "Nothing found :(!";
+        //return imageQuery.toString();
+        return userImages.toString();
     }
-    
+
     @POST
     @Path("findUserByPK")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
@@ -144,10 +166,8 @@ public class GenericResource {
         em.getTransaction().commit();
         emf.close();
         return "Nothing was found!";
-    } 
-    
-   
-   
+    }
+
     // query to search for users
     @GET
     @Path("get_users")
@@ -157,13 +177,10 @@ public class GenericResource {
             emf = Persistence.createEntityManagerFactory("ImageRektPU");
             em = emf.createEntityManager();
             for (User p : (List<User>) em.createNamedQuery("User.findAll").getResultList()) {
-                i++;
-                this.userList = this.userList + p.getUname() + p.getUemail() + this.newLine;
-                return ("Total users found: " + i + " username found: " + p.getUname()+ " with email " + p.getUemail());
+                return ("Total users found: " + " username found: " + p.getUname() + " with email " + p.getUemail() + "<br>" + "</br>" + "\n");
             }
             em.close();
             emf.close();
-           // return "" + this.userListStart + this.userList;
         } catch (Exception e) {
             return "Something is broken, again. : " + e;
         }
@@ -172,6 +189,7 @@ public class GenericResource {
 
     /**
      * PUT method for updating or creating an instance of GenericResource
+     *
      * @param content representation for the resource
      * @return an HTTP response with content of the updated or created resource.
      */
